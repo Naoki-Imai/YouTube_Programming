8ef8e713bab0404d3f482747813fed8f82d1b046
Naoki-Imai </Users/imainaoki/Library/Mobile>
2020-05-14 23:27:51 +0900

    edit,delete,add

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,13 +12,35 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new(movie_params)
-    @movie.user_id = current_user.id
-    @movie.save
-    redirect_to movie_path(@movie)
+    @movie = current_user.movies.build(movie_params)
+    @movie.url = @movie.url[32..-1]
+    if @movie.save
+      redirect_to movie_path(@movie), notice: "投稿しました"
+    else
+      render :new
+    end
   end
 
   def edit
+    @movie = Movie.find(params[:id])
+    if @movie.user != current_user
+      redirect_to movie_path(@movie), alert: "不正なアクセスです"
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie), notice: "編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    movie = Movie.find(params[:id])
+    movie.destroy
+    redirect_to user_path(movie.user), notice: "削除しました"
   end
 
   private
